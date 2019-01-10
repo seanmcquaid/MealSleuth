1be56e0f078eff166ba88c2d57fca40d07f077cd
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-10 16:11:15 -0500

    adding different api keys

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const zomatoKey = `b23b754b2022b0ede4349def62580bae`;
-const placesKey = `AIzaSyB8S65sZlmJq1Yh6pLBEAcY_-oLCSXoJqo`;
-const distanceKey = `AIzaSyDVLOPKZFYBGML1gOkPwBObb58xCHTKiUc`;
+const placesKey = `AIzaSyBhEyPG1pqRNT2FOdpiMX_2zRDYgY8Y1W0`;
+const distanceKey = `AIzaSyBhEyPG1pqRNT2FOdpiMX_2zRDYgY8Y1W0`;
